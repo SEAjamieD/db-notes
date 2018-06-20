85371a7ab1df40aaa235b7c2696fedd6bea4b676
SEAjamieD <jamie.degnan@gmail.com>
2018-06-20 13:39:13 -0700

    add anime.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import anime from 'animejs';
 import styled from 'styled-components';
 import './App.css';
 
@@ -8,32 +9,47 @@ const Notepad = styled.div`
   min-height: 90%;
   width: 90%;
   background: white;
+  h1 {
+    font-family: 'Rock Salt', cursive;
+    margin: 0;
+    padding: 5px 0;
+    text-align: center;
+  }
 `;
 
-const Title = styled.h1`
-  font-family: 'Rock Salt', cursive;
-  margin: 0;
-  padding: 5px 0;
-  text-align: center;
-`;
+// const Title = styled.h1`
+//   font-family: 'Rock Salt', cursive;
+//   margin: 0;
+//   padding: 5px 0;
+//   text-align: center;
+// `;
 
 const NoteInput = styled.input`
   width: 100%;
   font-size: 26px;
   height: 2em;
-  padding: 4px 2px 4px 5px
+  padding: 4px 2px 4px 5px;
+  border: none;
+  border-radius: 0;
 `;
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      test: ''
+      test: '',
+      ready: false
     }
   }
 
   componentDidMount() {
     this.fetchNotes();
+  }
+
+  componentDidUpdate() {
+    if (this.state.test) {
+      this.animateIn();
+    }
   }
 
   fetchNotes = () => {
@@ -47,7 +63,10 @@ class App extends Component {
       })
       .then((data) => {
         console.log('success');
-        this.setState({test: data})
+        this.setState({
+          test: data,
+          ready: true,
+        })
       })
       .catch((error) => {
         console.log('Error', error);
@@ -55,16 +74,41 @@ class App extends Component {
   }
 
   render() {
+    if (this.state.ready === true) {
+
+      return (
+        <div className="App">
+          <Notepad innerRef={el => (this.notepad = el)}>
+            <h1>NOTES</h1>
+            <NoteInput type="text" />
+          </Notepad>
+
+        </div>
+      );
+
+    }
+
     return (
       <div className="App">
-        <Notepad>
-          <Title>To Do</Title>
-          <NoteInput type="text" />
-        </Notepad>
 
       </div>
     );
   }
+
+
+
+ animateIn() {
+   const { notepad } = this;
+   anime({
+     targets: notepad,
+     opacity: [0,1],
+     translateY: [-1000, 0],
+     easing: "easeOutSine",
+     duration: 2000
+   })
+ }
+
+
 }
 
 export default App;

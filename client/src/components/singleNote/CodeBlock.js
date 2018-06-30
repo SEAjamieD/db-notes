0e69338cac4faeaf6cdd53ee78581ce4fd4c5ca3
SEAjamieD <jamie.degnan@gmail.com>
2018-06-29 20:15:07 -0700

    syntax highlighting working but needs styling

--- client/src/components/singleNote/CodeBlock.js
+++ client/src/components/singleNote/CodeBlock.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import './dracula.css';
 const hljs = window.hljs
 
 
 class CodeBlock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.setRef = this.setRef.bind(this)
+  }
+
+  setRef(el) {
+    this.codeEl = el
+  }
+
+  componentDidMount() {
+    this.highlightCode()
+  }
+
+  componentDidUpdate() {
+    this.highlightCode()
+  }
+
+  highlightCode() {
+    hljs.highlightBlock(this.codeEl)
+  }
 
   render() {
     return (
       <pre>
-        <code>
+        <code ref={this.setRef}>
+          {this.props.value}
         </code>
       </pre>
     );
